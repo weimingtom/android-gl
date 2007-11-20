a3a4708e7124466424891554e6d1312dfe0af46c
brendan.d.burns <brendan.d.burns@ce7ef4b6-353f-0410-b698-81db55532942>
2007-11-20 22:03:12 +0000

    fixed texture loading a little, changed graphic for blending

--- trunk/AndroidGL/src/edu/union/GLTutorialBase.java
+++ trunk/AndroidGL/src/edu/union/GLTutorialBase.java
@@ -15,7 +15,7 @@ import android.view.View;
 public class GLTutorialBase extends View {
 	protected OpenGLContext glContext;
 	protected ViewAnimator animator;
-	
+
 	/**
 	 * Make a direct NIO FloatBuffer from an array of floats
 	 * @param arr The array
@@ -43,37 +43,59 @@ public class GLTutorialBase extends View {
 		ib.position(0);
 		return ib;
 	}
+
+	/**
+	 * Create a texture and send it to the graphics system
+	 * @param gl The GL object
+	 * @param bmp The bitmap of the texture
+	 * @param reverseRGB Should the RGB values be reversed?  (necessary workaround for loading .pngs...)
+	 * @return The newly created identifier for the texture.
+	 */
+	protected static int loadTexture(GL10 gl, Bitmap bmp) {
+		return loadTexture(gl, bmp, false);
+	}
 	
 	/**
 	 * Create a texture and send it to the graphics system
 	 * @param gl The GL object
 	 * @param bmp The bitmap of the texture
+	 * @param reverseRGB Should the RGB values be reversed?  (necessary workaround for loading .pngs...)
 	 * @return The newly created identifier for the texture.
 	 */
-	protected static int loadTexture(GL10 gl, Bitmap bmp) {
+	protected static int loadTexture(GL10 gl, Bitmap bmp, boolean reverseRGB) {
 		ByteBuffer bb = ByteBuffer.allocateDirect(bmp.height()*bmp.width()*4);
 		bb.order(ByteOrder.nativeOrder());
 		IntBuffer ib = bb.asIntBuffer();
-		
-		for (int y=0;y<bmp.height();y++)
+
+		for (int y=bmp.height()-1;y>-1;y--)
 			for (int x=0;x<bmp.width();x++) {
+				if (reverseRGB) {
+					int px = bmp.getPixel(x,y);
+					int alpha = (px & 0xFF000000) >> 24;
+					int red = (px & 0xFF0000)>>16;
+					int green = (px & 0xFF00)>>8;
+					int blue = (px & 0xFF);
+					ib.put((alpha << 24) | (blue << 16) | (green<<8) | (red));
+				}
+				else {
 					ib.put(bmp.getPixel(x,y));
+				}
 			}
 		ib.position(0);
 		bb.position(0);
-		
+
 		int[] tmp_tex = new int[1];
-		
+
 		gl.glGenTextures(1, tmp_tex, 0);
 		int tex = tmp_tex[0];
 		gl.glBindTexture(GL10.GL_TEXTURE_2D, tex);
 		gl.glTexImage2D(GL10.GL_TEXTURE_2D, 0, GL10.GL_RGBA, bmp.width(), bmp.height(), 0, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, bb);
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-	
+
 		return tex;
 	}
-	
+
 	/**
 	 * Constructor
 	 * @param c The View's context.
@@ -81,7 +103,7 @@ public class GLTutorialBase extends View {
 	public GLTutorialBase(Context c) {
 		this(c, -1);
 	}
-	
+
 	/**
 	 * Constructor for animated views
 	 * @param c The View's context
@@ -94,7 +116,7 @@ public class GLTutorialBase extends View {
 			animator = new ViewAnimator(this, fps);
 		}
 	}
-	
+
 	@Override
 	protected void onAttachedToWindow() {
 		if (animator != null) {
@@ -112,5 +134,5 @@ public class GLTutorialBase extends View {
 		}
 		super.onDetachedFromWindow();
 	}
-	
+
 }
--- trunk/AndroidGL/src/edu/union/R.java
+++ trunk/AndroidGL/src/edu/union/R.java
@@ -12,8 +12,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int block=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int zeus=0x7f020002;
+        public static final int glass=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int zeus=0x7f020003;
     }
     public static final class id {
         public static final int text=0x7f050000;
